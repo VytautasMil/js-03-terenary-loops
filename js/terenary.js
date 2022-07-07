c2da0ec8cbbbf84d44e81d979e8550e52b75e0a1
Vytautas <vyckelis@gmail.com>
2022-07-07 14:49:12 +0300

    terenary

--- js/terenary.js
+++ js/terenary.js
@@ -1,1 +1,34 @@
-console.log
+'use strict';
+console.log('terenary');
+
+// <salyga> ? <jei salyga true> : <jei salyga false>
+
+let hours = 15;
+
+// hours = 3;
+
+// let laikas = hours < 6 ? 'yra naktis' : 'yra diena';
+let laikas;
+if (hours < 6) {
+  laikas = 'yra naktis';
+} else {
+  laikas = 'yra diena';
+}
+
+console.log('laikas ===', laikas);
+
+let isDarkModeOn = false;
+const darkColor = '#333';
+const lightColor = '#fff';
+
+isDarkModeOn = true;
+
+if (isDarkModeOn === true) {
+  document.body.style.backgroundColor = darkColor;
+  document.body.style.color = lightColor;
+} else {
+  document.body.style.backgroundColor = lightColor;
+  document.body.style.color = darkColor;
+}
+
+// document.body.style.backgroundColor = isDarkModeOn === true ? darkColor : lightColor;
